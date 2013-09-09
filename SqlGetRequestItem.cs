5ce87744edc7c2efb0f34871fea3387f89b2d188
sgardini <stefano@gestionaleamica.com>
2013-09-09 08:42:50 +0200

    Added SqlGetRequestItem class

---
+++ SqlGetRequestItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using DataAccess;
+
+namespace Amica.Data
+{
+    public class SqlGetRequestItem : GetRequestItem
+    {
+        public string DataSourcePassword { get; set; }
+    }
+}
